6b094c307cefa35206fd70ef6e5db7e8e1011488
Thomas Cuvillier <titiomm@gmail.com>
2021-12-19 20:12:30 +0100

    Update 2_FlatPriceERC721_migration.js

--- migrations/2_FlatPriceERC721_migration.js
+++ migrations/2_FlatPriceERC721_migration.js
@@ -1,5 +1,6 @@
+const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 const FlatPriceERC721 = artifacts.require("FlatPriceERC721");
 
-module.exports = function (deployer) {
-  deployer.deploy(FlatPriceERC721, "TSquirrel NFT", "TSQRNFT", "2500");
+module.exports = async function (deployer) {
+  const instance = await deployProxy(FlatPriceERC721, ["TSquirrel NFT", "TSQRNFT", "30000000000000000000", 2500], { deployer });
 };
