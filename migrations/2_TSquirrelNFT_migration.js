d68f48ab7042ef2ef100793a33f504ce72ea1432
Thomas Cuvillier <titiomm@gmail.com>
2022-02-18 19:34:16 +0100

    Update 2_TSquirrelNFT_migration.js

--- migrations/2_TSquirrelNFT_migration.js
+++ migrations/2_TSquirrelNFT_migration.js
@@ -3,5 +3,5 @@ const TSquirrelNFT = artifacts.require("TSquirrelNFT");
 
 module.exports = async function (deployer) {
   const metadata = []; // HIDE IN REPO
-  const instance = await deployProxy(TSquirrelNFT, ["TSquirrel NFT", "TSQRNFT", "30000000000000000000", 150, metadata], { deployer });
+  const instance = await deployProxy(TSquirrelNFT, ["TSquirrel NFT", "TSQRLNFT", "30000000000000000000", 150, metadata], { deployer });
 };
